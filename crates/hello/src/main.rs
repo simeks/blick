6adb8d0447bd585ebf1c41b2b67a97723abcdbdb
Simon Ekström <simon.ekstrom@gmail.com>
2023-02-28 23:07:40 +0100

    Window

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -1,4 +1,26 @@
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{EventLoop};
+
 
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = winit::window::WindowBuilder::new()
+        .with_title("blick")
+        .with_inner_size(winit::dpi::LogicalSize::new(800, 600))
+        .build(&event_loop)
+        .expect("Failed to create window");
+
+    loop {
+        event_loop.run(move |event, _, control_flow| {
+            match event {
+                Event::WindowEvent { event, .. } => match event {
+                    WindowEvent::CloseRequested => {
+                        *control_flow = winit::event_loop::ControlFlow::Exit;
+                    }
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
+    }
 }
